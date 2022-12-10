e53c184cf6c084aa212c6deb0f570bc893cfd78a
Emeka1993 <brolin933@gmail.com>
2022-12-10 13:03:54 +0000

    commit

--- server.js
+++ server.js
@@ -5,3 +5,8 @@ app.listen(3001, function () {
 console.log("Server is listenting on port 3001. ready to accept request!");
 });
 
+// req is the Request object, res is the Response object
+// (these are just variable names, they can be anything but it's a convention to call them req and res)
+app.get("/", function (req, res) {
+    res.send("Hello World!");
+  });
